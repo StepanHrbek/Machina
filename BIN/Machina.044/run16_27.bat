44e72ce3b7f94ebbcfc33db621e673a24ef0e1db
Stepan Hrbek <stepan.git@lightsprint.com>
2015-11-10 15:52:08 +0100

    Remove unnecessary .bat files.
    They were part of live CD/DVD with several GB of diskmags, they had no purpose here.

--- BIN/Machina.044/run16_27.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-rem   Spusti Masinu 0.44 z CD, na disk neuklada nic.
-rem   Pouze pro prohlizeni projektu verze 0.xx (Pareniste 5-27).
-
-cd ..\..\archiv
-..\machina\machina.044\diskmag.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
-cd ..\machina\machina.044
